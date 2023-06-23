b9ba86e9fd35308ab885b329b95f7c554840ddb1
arifrahmadi23 <muhammad231arif@gmail.com>
2023-06-23 11:05:01 +0700

    //jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array

--- CircularQueues_171/CircularQueues_171.cpp
+++ CircularQueues_171/CircularQueues_171.cpp
@@ -75,8 +75,21 @@ public:
 		//jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
 		if (FRONT_position <= REAR_position) {
 			while (FRONT_position <= REAR_position) {
-				cout << queue_array[FRONT_position] <<
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
 			}
+			cout << endl;
+		}
+		else {
+			//jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array
+			while (FRONT_position < n - 1) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			//ierasi dari awal array hingga REAR
 		}
 
 	}
